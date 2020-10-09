af07118c9644d056eb2761165eafd6f232eb5480
russi115 <shamanse2001@gmail.com>
2020-10-09 07:49:56 -0400

    make it better with heroku

--- src/index.py
+++ src/index.py
@@ -4,7 +4,6 @@ import datetime
 from urllib import parse, request
 import re
 import os
-#from boto.s3.connection import S3Connection
  
 
 
@@ -104,7 +103,6 @@ async def my_message(message): pass
 bot.add_listener(my_message, 'busy_message')
 
 bot.run(os.getenv("TOKEN"))
-#bot.run(S3Connection(os.environ['TOKEN']))
 
 
 # https://discordapp.com/developers/applications/688292551211745280/oauth2s
